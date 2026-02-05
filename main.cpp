f3452b4a9d1f6fb5565d1fb8721b153007c4973a
awhite76 <awhite76@calpoly.edu>
2026-02-05 10:46:17 -0800

    Main framework

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <alsa/asoundlib.h>
+
+#define T_MS 15
+#define SAMPLE_RATE 48000
+
+using namespace std;
+
+int main()
+{
+    // configure ALSA for audio input and output
+
+    // load reference audios (vocals and music)
+
+    while (1)
+    {
+        // read audio input
+
+        // process audio input
+
+        // output audio
+    }
+    return 0;
+}
