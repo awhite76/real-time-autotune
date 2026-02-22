4c25e67835da1b73a4355423aa413974c81217bf
qcollinsw <collinsworthquincy@gmail.com>
2026-02-22 14:19:42 -0800

    Hello

--- autotune/main.cpp
+++ autotune/main.cpp
@@ -196,7 +196,7 @@ int main(int argc, char **argv)
     fftwf_complex *X; fftwf_complex *Y; int num_windows;
     int Hs; int out_L; fftwf_plan p_r2c; fftwf_plan p_c2r;
 
-    float time_stretch = 1.0f;
+    float time_stretch = 3.0f;
 
     cout << "Prevocoder\n";
 
